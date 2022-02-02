cddb9ccf80be0c7d488f819a64f999843458bb89
agMalek <albermaleh.99@gmail.com>
2022-02-02 20:53:51 -0300

    eje1

---
+++ js/eje1.js
@@ -0,0 +1,19 @@
+/* 
+    ------------------- EJERCICIO 1 ----------------
+    Por favor, haz un loop de 1 a 17. 
+*/
+
+const inicio = 1
+const final = 17
+
+/* CON FOR */
+for(let i = inicio; i<=final; i++){
+    console.log(i)
+}
+
+/* CON WHILE */
+let contador = 1
+while(contador<=final){
+    console.log(contador)
+    contador++
+}
