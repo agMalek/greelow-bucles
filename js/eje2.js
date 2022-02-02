74c5fd2247ce04e607d805d43df625e94f35e0cb
agMalek <albermaleh.99@gmail.com>
2022-02-02 20:58:44 -0300

    eje2

---
+++ js/eje2.js
@@ -0,0 +1,20 @@
+/* 
+    ------------------- EJERCICIO 2 ----------------
+   Cuenta del 7 al 12 con un loop e imprime cada número en la consola.
+
+*/
+
+const inicio = 7
+const final = 12
+
+/* CON FOR */
+for(let i = inicio; i<=final; i++){
+    console.log(i)
+}
+
+/* CON WHILE */
+let contador = 7
+while(contador<=final){
+    console.log(contador)
+    contador++
+}
